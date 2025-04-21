f089cb89af8352966c0fe7e00cfec2e78fa6705d
Janardhan <jan.cj84@gmail.com>
2025-04-21 22:53:32 +0530

    feat: add logging and request handling middleware with Winston and Morgan

---
+++ src/config/logger.js
@@ -0,0 +1,48 @@
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const { combine, timestamp, printf } = format;
+
+// Define log format
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
+// Create a logger instance
+const logger = createLogger({
+  level: 'info', // Set the default log level
+  format: combine(
+    timestamp(), // Add timestamp to logs
+    logFormat // Apply custom log format
+  ),
+  transports: [
+    // Log to console with colorized output
+    new transports.Console({
+      format: format.combine(
+        format.colorize(), // Colorize output
+        format.simple() // Use simple format
+      )
+    }),
+    // Log to a file
+    new DailyRotateFile({
+      filename: 'logs/%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m', // Rotate logs if they exceed 20MB
+      maxFiles: '14d' // Keep logs for 14 days
+    })
+  ]
+});
+
+// Add a custom log level for warnings
+logger.add(
+  new transports.Console({
+    format: format.combine(
+      format.colorize(), // Colorize output
+      format.simple() // Use simple format
+    ),
+    level: 'warn' // Set log level to warn
+  })
+);
+
+export default logger;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
+import logger from './config/logger.js';
+import { errorHandler,successHandler } from './config/morgan.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
@@ -9,7 +11,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(successHandler);
+app.use(errorHandler);
 app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
@@ -18,5 +21,6 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is listing to ${PORT}`);
+  logger.info(`Listening to port ${process.env.PORT}`);
   connectDB();
 });
