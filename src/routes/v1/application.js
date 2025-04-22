71212b1f101090e80bdd8b6aa051dbd75b1e53a2
Janardhan <jan.cj84@gmail.com>
2025-04-22 22:11:38 +0530

    fix: correct HTTP methods for application routes

--- src/routes/v1/application.js
+++ src/routes/v1/application.js
@@ -2,14 +2,15 @@ import express from 'express';
 
 import {
   createApplicationController,
+  deleteApplicationController,
   getApplicationByIdController
 } from '../../controllers/jobApplication.js';
 import { isAuthenticated } from '../../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', isAuthenticated, getApplicationByIdController);
+router.get('/', isAuthenticated, getApplicationByIdController);
 router.put('/:applicationId', isAuthenticated, createApplicationController);
 router.post('/', isAuthenticated, createApplicationController);
-router.delete('/:applicationId', isAuthenticated, createApplicationController);
+router.delete('/:applicationId', isAuthenticated, deleteApplicationController);
 export default router;
