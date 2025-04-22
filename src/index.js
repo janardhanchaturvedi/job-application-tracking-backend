a374e4c52514ef6d9ba2d846539fadc022cc7d06
Janardhan <jan.cj84@gmail.com>
2025-04-22 21:10:39 +0530

    feat: add CORS middleware for handling cross-origin requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
 import logger from './config/logger.js';
-import { errorHandler,successHandler } from './config/morgan.js';
+import { errorHandler, successHandler } from './config/morgan.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(successHandler);
 app.use(errorHandler);
 app.use('/api', apiRouter);
